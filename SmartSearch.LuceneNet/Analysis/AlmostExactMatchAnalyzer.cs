06ce41000d425a55b42157108521bd22683e274a
Phillippe Santana <phillippelevidad@gmail.com>
2023-05-04 18:01:33 -0300

    Added special analyzer to perfom almost-exact matches

---
+++ SmartSearch.LuceneNet/Analysis/AlmostExactMatchAnalyzer.cs
@@ -0,0 +1,27 @@
+﻿using Lucene.Net.Analysis.Standard;
+using Lucene.Net.Analysis;
+using Lucene.Net.Util;
+using System.IO;
+using Lucene.Net.Analysis.Core;
+using Lucene.Net.Analysis.Miscellaneous;
+using Lucene.Net.Analysis.Pattern;
+using System.Text.RegularExpressions;
+
+namespace SmartSearch.LuceneNet.Analysis
+{
+    internal class AlmostExactMatchAnalyzer : Analyzer
+    {
+        protected override TokenStreamComponents CreateComponents(
+            string fieldName,
+            TextReader reader
+        )
+        {
+            Tokenizer source = new StandardTokenizer(LuceneVersion.LUCENE_48, reader);
+            TokenStream filter = new LowerCaseFilter(LuceneVersion.LUCENE_48, source);
+            filter = new ASCIIFoldingFilter(filter);
+            filter = new PatternReplaceFilter(filter, new Regex(@"\p{P}"), "", true);
+            filter = new SingularPortugueseFilter(filter);
+            return new TokenStreamComponents(source, filter);
+        }
+    }
+}
